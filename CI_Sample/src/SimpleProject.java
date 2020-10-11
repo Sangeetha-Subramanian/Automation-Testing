9cf4d9196491128a0715fb1eedd4701ea62ced28
Sangeetha-Subramanian <sangeethaapr98@gmail.com>
2020-10-11 07:55:26 +0530

    added print statement

--- CI_Sample/src/SimpleProject.java
+++ CI_Sample/src/SimpleProject.java
@@ -3,7 +3,11 @@ public class SimpleProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("selenium");
 		
+		System.out.println("ci integration");
+		
+		System.out.println("successfully integrated");
 
 	}
 
